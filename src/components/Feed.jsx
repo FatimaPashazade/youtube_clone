f49cf7d47c89584acb32906cd11f82926e4e7571
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-08-01 13:54:52 +0200

    Fetching API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from "@mui/material";
 import {Sidebar, Videos } from "./";
-
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
+
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
     <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
