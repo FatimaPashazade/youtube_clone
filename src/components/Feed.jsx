ac0dade2eb7d5c903c4698708a868fafbbf344b7
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-08-09 12:10:03 +0200

    Updates

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,17 +8,19 @@ const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
   const [videos, setVideos] = useState(null);
 
+
   useEffect(() => {
     setVideos(null);
 
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    fetchFromAPI(`search?part=snippet&q=$
+    {selectedCategory}`)
       .then((data) => setVideos(data.items))
     }, [selectedCategory]);
-
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <Sidebar selectedCategory={selectedCategory}
+         setSelectedCategory={setSelectedCategory} />
 
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2022 JSM Media
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,14 +1,18 @@
-const axios = require('axios');
+import axios from "axios";
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
+  url: BASE_URL,
   params: {
-    part: 'snippet',
-    videoId: 'M7FIvfx5J10'
+    relatedToVideoId: '7ghhRHRP6t4',
+    part: 'id,snippet',
+    type: 'video',
+    maxResults: '50'
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': process.env.
+    REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
@@ -16,6 +20,6 @@ const options = {
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-
+  console.log(data)
   return data;
 };
