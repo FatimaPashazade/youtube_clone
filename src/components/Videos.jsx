0ddc7a44d0c2b9f5b0a56813d3ab258dc88cd1bf
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-08-11 12:31:02 +0200

    Updates

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,7 +5,7 @@ import { ChannelCard, Loader, VideoCard } from "./";
 
 const Videos = ({ videos, direction }) => {
   if(!videos?.length) return <Loader />;
-
+console.log(videos)
   return (
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       {videos.map((item, idx) => (
