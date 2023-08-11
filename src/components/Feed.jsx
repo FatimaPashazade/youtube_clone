fa878d8f5845f25e5b57aeef1a51786dd5d87996
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-08-11 12:30:14 +0200

    Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,14 +8,13 @@ const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
   const [videos, setVideos] = useState(null);
 
-
   useEffect(() => {
     setVideos(null);
 
-    fetchFromAPI(`search?part=snippet&q=$
-    {selectedCategory}`)
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
       .then((data) => setVideos(data.items))
     }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
